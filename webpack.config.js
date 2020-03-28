1e37164692be6252145a2fa0d5d30ea07c36cfcd
Gabriel Lima <g.lima@payvision.com>
2020-03-28 16:09:38 +0100

    chore: fix build task

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
 const path = require('path');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const ImageMinPlugin = require('imagemin-webpack-plugin').default;
-const getHTMLPluginConfig = require('./webpack/html-plugin.webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPartialsPlugin = require('html-webpack-partials-plugin');
 
 const paths = {
+  root: '.',
   src: 'src',
   dist: 'dist',
   assets: 'src/assets',
@@ -21,7 +21,7 @@ const paths = {
 const config = {
   context: path.join(__dirname, './'),
   entry: {
-    main: [`./${paths.components}/home/home.ts`, `./${paths.src}/styles.scss`]
+    main: [`${path.join(__dirname, paths.components)}/home/home.ts`, `${path.join(__dirname, paths.src)}/styles.scss`]
   },
   output: {
     filename: 'javascript.min.js',
@@ -54,49 +54,30 @@ const config = {
     ]
   },
   plugins: [
-    new CleanWebpackPlugin({
-      cleanStaleWebpackAssets: false,
-      verbose: true
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'stylesheets.min.css'
-    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({filename: 'stylesheets.min.css'}),
     new CopyWebpackPlugin([
       {
-        from: path.resolve(__dirname, `${paths.images}/me.jpg`),
-        to: path.resolve(__dirname, 'dist'),
-        toType: 'dir'
-      },
-      {
-        from: path.resolve(__dirname, `${paths.images}/*.{jpg,jpeg,png,gif,svg}`),
-        to: path.resolve(__dirname, 'dist/assets/images'),
-        toType: 'dir'
+        context: 'src/',
+        from: path.join(__dirname, `${paths.images}/*.{jpg,jpeg,png,gif,svg}`),
+        to: path.join(__dirname, paths.dist),
+        toType: 'dir',
+        force: true
       }
     ]),
     new ImageMinPlugin({test: /\.(jpg|jpeg|png|gif|svg)$/i}),
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      template: path.join(__dirname, './index.html')
+      template: path.join(__dirname, `${paths.root}/index.html`)
     }),
     new HtmlWebpackPartialsPlugin([
-      {
-        path: path.join(__dirname, './src/components/intro/intro.html')
-      },
-      {
-        path: path.join(__dirname, './src/components/social-links/social-links.html')
-      },
-      {
-        path: path.join(__dirname, './src/components/footer/footer.html')
-      }
+      {path: `${path.join(__dirname, paths.components)}/intro/intro.html`},
+      {path: `${path.join(__dirname, paths.components)}/social-links/social-links.html`},
+      {path: `${path.join(__dirname, paths.components)}/footer/footer.html`}
     ])
   ],
   optimization: {
-    minimizer: [
-      new TerserPlugin({
-        parallel: true
-      }),
-      new OptimizeCssAssetsPlugin({})
-    ]
+    minimizer: [new TerserPlugin({parallel: true}), new OptimizeCssAssetsPlugin({})]
   }
 };
 
