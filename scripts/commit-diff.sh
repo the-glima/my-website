c170e4d079f4fcd2218c8f936bad63d3aee7ac20
Gabriel Lima <email.gabriel.lima@gmail.com>
2020-04-06 04:14:08 +0200

    ci: fix release worflow

---
+++ scripts/commit-diff.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+RUN_COMMAND=$1
+DIFF_PATH=$2
+
+function commitDiff() {
+  local RESULT=$(git diff HEAD~1 --name-only > /dev/null 2>&1)
+
+  if [[ -n "${RESULT}" ]]; then
+    echo "${RESULT}"
+  else
+    echo "No files were changed"
+  fi
+
+  exit 0
+}
+
+commitDiff
