d929ccb8802f946398df8ad085beec4431afc166
Gabriel Lima <email.gabriel.lima@gmail.com>
2020-04-13 01:06:57 +0200

    chore: revert eject

--- src/settings.ts
+++ src/settings.ts
@@ -12,7 +12,8 @@ export const settings = {
     logo: 'GitHub'
   },
   intro: {
-    bio: 'Working in web development for almost 8 years with solid experience developing web applications, creating rich UI components, building RESTful APIs and improving development workflow (CI/CD and product infrastructure). Always learning and keeping track of modern technologies and new standards.',
+    bio:
+      'Working in web development for almost 8 years with solid experience developing web applications, creating rich UI components, building RESTful APIs and improving development workflow (CI/CD and product infrastructure). Always learning and keeping track of modern technologies and new standards.',
     subTitleOptions: [
       'Front-End Developer',
       'Front-End Engineer',
@@ -29,9 +30,12 @@ export const settings = {
   },
   footer: {
     links: {
-      brazil: 'https://www.google.com/search?q=p%C3%A3o+de+queijo&rlz=1C1GCEA_enES841ES841&oq=p%C3%A3o+de+quu&aqs=chrome.1.69i57j0l7.8238j0j7&sourceid=chrome&ie=UTF-8',
-      ireland: 'https://www.google.com/search?rlz=1C1GCEA_enES841ES841&ei=l0RkXs7RLJbolwSHlJmYCw&q=guiness+pint&oq=guiness+pint&gs_l=psy-ab.3..0i10l10.113793.114999..115186...0.0..0.141.1332.7j6......0....1..gws-wiz.......0i7i30j0j0i67.cx-4hy65v7I&ved=0ahUKEwiO-ZC914noAhUW9IUKHQdKBrMQ4dUDCAs&uact=5',
-      spain: 'https://www.google.com/search?rlz=1C1GCEA_enES841ES841&ei=DEVkXtyZDMm4aZqAu5AG&q=j%C3%A1mon+iberico&oq=j%C3%A1mon+&gs_l=psy-ab.3.0.0j0i30l4j0i5i30j0i30l3j0i5i30.25040.26774..28635...0.0..0.121.646.1j5......0....1..gws-wiz.......0i273.Wk5_W6D8jGA'
+      brazil:
+        'https://www.google.com/search?q=p%C3%A3o+de+queijo&rlz=1C1GCEA_enES841ES841&oq=p%C3%A3o+de+quu&aqs=chrome.1.69i57j0l7.8238j0j7&sourceid=chrome&ie=UTF-8',
+      ireland:
+        'https://www.google.com/search?rlz=1C1GCEA_enES841ES841&ei=l0RkXs7RLJbolwSHlJmYCw&q=guiness+pint&oq=guiness+pint&gs_l=psy-ab.3..0i10l10.113793.114999..115186...0.0..0.141.1332.7j6......0....1..gws-wiz.......0i7i30j0j0i67.cx-4hy65v7I&ved=0ahUKEwiO-ZC914noAhUW9IUKHQdKBrMQ4dUDCAs&uact=5',
+      spain:
+        'https://www.google.com/search?rlz=1C1GCEA_enES841ES841&ei=DEVkXtyZDMm4aZqAu5AG&q=j%C3%A1mon+iberico&oq=j%C3%A1mon+&gs_l=psy-ab.3.0.0j0i30l4j0i5i30j0i30l3j0i5i30.25040.26774..28635...0.0..0.121.646.1j5......0....1..gws-wiz.......0i273.Wk5_W6D8jGA'
     }
   }
 };
