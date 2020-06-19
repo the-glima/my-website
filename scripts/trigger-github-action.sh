bbf3d552aa164913a8976384ec639afe94feeef5
Gabriel Lima <email.gabriel.lima@gmail.com>
2020-06-19 14:23:30 +0200

    refactor: add repo name var to trigger action script

--- scripts/trigger-github-action.sh
+++ scripts/trigger-github-action.sh
@@ -1,19 +1,16 @@
-#!/usr/bin/env bash
+# !/usr/bin/env bash
 
-# Manually Trigger A GitHub Actions Workflow: 
-# https://goobar.io/2019/12/07/manually-trigger-a-github-actions-workflow/
-
+REPO_NAME=$(grep REPO_NAME .env | cut -d '=' -f 2-)
 GITHUB_TOKEN=$(grep GITHUB_TOKEN .env | cut -d '=' -f 2-)
 
 function triggerGithubAction() {
-  echo "Running Manual GitHub Action Trigger, check:"
-  echo "https://github.com/the-glima/my-website/actions"
+  echo "GitHub: Running Manual Action Trigger for: ${REPO_NAME}"
 
   curl -H "Accept: application/vnd.github.everest-preview+json" \
       -H "Authorization: token ${GITHUB_TOKEN}" \
       --request POST \
       --data '{"event_type": "manual-action-trigger""}' \
-      https://api.github.com/repos/the-glima/my-website/dispatches 
+      https://api.github.com/repos/${REPO_NAME}/dispatches
 }
 
-triggerGithubAction 
+triggerGithubAction
