b8bf668b6f53793d07cfe875c02d140007b31791
Jean Pimentel <jpimentel@ciandt.com>
2020-10-01 20:20:14 -0300

    feat: add i18n to countries

--- src/containers/work/WorkData.ts
+++ src/containers/work/WorkData.ts
@@ -12,7 +12,7 @@ export const WorkData = (t: any): WorkModel[] => [
     title: 'Ciklum',
     position: t('position.senior-front-end-developer'),
     url: 'https://www.ciklum.com/',
-    country: 'Spain',
+    country: t('countries.spain'),
     year: '2020',
     status: 'current'
   },
@@ -20,41 +20,41 @@ export const WorkData = (t: any): WorkModel[] => [
     title: 'Payvision',
     position: t('position.software-developer'),
     url: 'https://www.payvision.com',
-    country: 'Spain',
+    country: t('countries.spain'),
     year: '2019'
   },
   {
     title: 'Webdoctor',
     position: t('position.software-developer'),
     year: '2017',
-    country: 'Ireland',
+    country: t('countries.ireland'),
     url: 'https://www.webdoctor.ie/'
   },
   {
     title: 'Neoway',
     position: t('position.javascript-developer'),
-    country: 'Brazil',
+    country: t('countries.brazil'),
     year: '2016',
     url: 'https://www.neoway.com.br/'
   },
   {
     title: 'Div64',
     position: t('position.front-end-developer'),
-    country: 'Brazil',
+    country: t('countries.brazil'),
     year: '2015',
     url: 'https://www.usebeon.com/'
   },
   {
     title: 'EloGroup',
     position: t('position.front-end-developer'),
-    country: 'Brazil',
+    country: t('countries.brazil'),
     year: '2013',
     url: 'https://elogroup.com.br/'
   },
   {
     title: 'Bolt Brasil',
     position: t('position.front-end-developer'),
-    country: 'Brazil',
+    country: t('countries.brazil'),
     year: '2011',
     url: 'https://www.bolt.com.br/'
   }
