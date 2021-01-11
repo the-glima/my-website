41d9565a96c7c26204d4c77c8c1caebf937ca8cf
Gabriel Lima <email.gabriel.lima@gmail.com>
2021-01-11 18:16:49 +0100

    chore: fix worflow

--- scripts/trigger-github-action.sh
+++ scripts/trigger-github-action.sh
@@ -2,6 +2,7 @@
 
 REPO_NAME=$(grep REPO_NAME .env | cut -d '=' -f 2-)
 GITHUB_TOKEN=$(grep GITHUB_TOKEN .env | cut -d '=' -f 2-)
+BRANCH_NAME="${1:=main}"
 
 function triggerGithubAction() {
   echo "GitHub: Running Manual Action Trigger for: ${REPO_NAME}"
@@ -9,7 +10,7 @@ function triggerGithubAction() {
   curl -H "Accept: application/vnd.github.everest-preview+json" \
       -H "Authorization: token ${GITHUB_TOKEN}" \
       --request POST \
-      --data '{"event_type": "manual-action-trigger""}' \
+      --data '{"event_type": "manual-action-trigger", "client_payload": { "branch_name": '${BRANCH_NAME}'}}' \
       https://api.github.com/repos/${REPO_NAME}/dispatches
 }
 
