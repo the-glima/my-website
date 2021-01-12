991cc8979fcc9b40541f2aa626168f34ec75d94d
Gabriel Lima <email.gabriel.lima@gmail.com>
2021-01-12 17:00:21 +0100

    fix: workflow deploywq

--- scripts/trigger-github-action.sh
+++ scripts/trigger-github-action.sh
@@ -5,16 +5,13 @@ BRANCH_NAME="${1:=main}"
 
 function triggerGithubAction() {
   echo "GitHub: Running Manual Action Trigger"
-  # curl -H "Accept: application/vnd.github.everest-preview+json" \
-  #     -H "Authorization: token ${GITHUB_TOKEN}" \
-  #     --request POST \
-  #     --data '{"event_type": "manual-action-trigger", "client_payload": { "branch_name": '${BRANCH_NAME}'}}' \
-  #     https://api.github.com/repos/${REPO_NAME}/dispatches
+
   curl \
     -X POST \
     -H "Accept: application/vnd.github.v3+json" \
     -H "Authorization: token ${GITHUB_TOKEN}" \
-    https://api.github.com/repos/the-glima/my-website/actions/workflows/Manually%20Triggered/dispatches \
-    -d '{"ref": "'${BRANCH_NAME}'", "hello": "Hola", "world": "Mundo"}'
+    --data '{"ref": "'${BRANCH_NAME}'", "inputs": {"hello": "Hola", "world": "Mundo"}}' \
+    https://api.github.com/repos/the-glima/my-website/actions/workflows/manual-trigger.yml/dispatches
+}
 
 triggerGithubAction
