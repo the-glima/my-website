a469ab24b137c2541fedccb1368c757ae3e96149
Gabriel Lima <email.gabriel.lima@gmail.com>
2021-01-12 13:10:38 +0100

    fix: workflow deploywq

--- scripts/trigger-github-action.sh
+++ scripts/trigger-github-action.sh
@@ -6,10 +6,15 @@ BRANCH_NAME="${1:=main}"
 
 function triggerGithubAction() {
   echo "GitHub: Running Manual Action Trigger for: ${REPO_NAME}"
-
-  curl -H "Accept: application/vnd.github.everest-preview+json" \
-      -H "Authorization: token ${GITHUB_TOKEN}" \
-      --request POST \
-      --data '{"event_type": "manual-action-trigger", "client_payload": { "branch_name": '${BRANCH_NAME}'}}' \
-      https://api.github.com/repos/${REPO_NAME}/dispatches
+  # curl -H "Accept: application/vnd.github.everest-preview+json" \
+  #     -H "Authorization: token ${GITHUB_TOKEN}" \
+  #     --request POST \
+  #     --data '{"event_type": "manual-action-trigger", "client_payload": { "branch_name": '${BRANCH_NAME}'}}' \
+  #     https://api.github.com/repos/${REPO_NAME}/dispatches
+  curl \
+    -X POST \
+    -H "Accept: application/vnd.github.v3+json" \
+    -H "Authorization: token ${GITHUB_TOKEN}" \
+    https://api.github.com/repos/octocat/hello-world/actions/workflows/42/dispatches \
+    -d '{"ref": "'${BRANCH_NAME}'", "hello": "Hello", "world": "World"}'
 }
