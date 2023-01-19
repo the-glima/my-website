80f35f46e0d0cdbe0dc88dd9008d5f66b2018991
Gabriel Lima <email.gabriel.lima@gmail.com>
2023-01-19 12:59:46 -0300

    feat: add nexhealth in projects

--- src/containers/projects/ProjectsData.ts
+++ src/containers/projects/ProjectsData.ts
@@ -15,6 +15,13 @@ export const ProjectsData = (t: any): ProjectModel[] => [
     status: 'done'
   },
   {
+    name: 'NexHealth',
+    favicon: 'https://assets.website-files.com/62f7955338a7286dd4b633cc/6384d406d54e4266fa6690a3_NexHealth-Webclip.png',
+    url: 'https://www.nexhealth.com/',
+    description: t('projects.links.nexhealth'),
+    status: 'done'
+  },
+  {
     name: 'Has Health',
     favicon: 'https://www.hashealth.com/wp-content/uploads/2020/11/cropped-hashealth-favicon-2-32x32.png',
     url: 'https://www.hashealth.com/',
@@ -26,13 +33,6 @@ export const ProjectsData = (t: any): ProjectModel[] => [
     favicon: 'https://www.payvision.com/assets/icons/favicons/favicon-64.png',
     url: 'https://www.payvision.com/',
     description: t('projects.links.payvision'),
-    status: 'done'
-  },
-  {
-    name: 'Lottoland',
-    favicon: 'https://www.lottoland.com/en/skins/lottoland/images/ui/favicon-906cd6ceee89e2b3.ico',
-    url: 'https://www.lottoland.com/en',
-    description: t('projects.links.lottoland'),
     status: 'done'
   }
 ]
