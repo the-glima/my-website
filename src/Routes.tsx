884a5a1622e2fee2f272d52f326577f0f4f487d8
Gabriel Lima <email.gabriel.lima@gmail.com>
2023-01-30 22:45:59 -0300

    feat: remove mtg ranking

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,13 +2,11 @@ import React from "react";
 import { Route, BrowserRouter } from "react-router-dom";
 
 import Home from './pages/home/Home'
-import MTGRanking from './pages/mtg-ranking/MTGRanking';
 
 const Routes = () => {
    return(
       <BrowserRouter>
         <Route component= { Home }  path="/" exact />
-        <Route component={ MTGRanking }  path="/mtg-ranking" />
       </BrowserRouter>
    )
 }
