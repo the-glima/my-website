a77522a17b4ff9633c9a637aab7356e16ed044d4
Gabriel Lima <email.gabriel.lima@gmail.com>
2025-11-13 05:34:11 -0300

    fix: deploy action

--- scripts/trigger-github-action.sh
+++ scripts/trigger-github-action.sh
@@ -4,14 +4,13 @@ GITHUB_TOKEN=$(grep VITE_GH_TOKEN .env | cut -d '=' -f 2-)
 BRANCH_NAME="${1:-main}"
 
 function triggerGithubAction() {
-  echo "GitHub: Running Manual Action Trigger"
+  echo "GitHub: Running Deploy Trigger"
 
   curl \
     -X POST \
     -H "Accept: application/vnd.github.v3+json" \
     -H "Authorization: token ${GITHUB_TOKEN}" \
-    --data '{"ref": "'${BRANCH_NAME}'", "inputs": {"hello": "Olá", "world": "Mundo"}}' \
-    https://api.github.com/repos/the-glima/my-website/actions/workflows/manual-trigger.yml/dispatches
+    --data '{"ref": "'${BRANCH_NAME}'"}' \
+    https://api.github.com/repos/the-glima/my-website/dispatches \
+    -f event_type='deploy-trigger'
 }
-
-triggerGithubAction
