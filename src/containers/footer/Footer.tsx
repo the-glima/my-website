135815b14fa575a409e79159ac115a58f825c054
Gabriel Lima <email.gabriel.lima@gmail.com>
2025-11-25 21:54:20 -0300

    feat: add version in the footer

--- src/containers/footer/Footer.tsx
+++ src/containers/footer/Footer.tsx
@@ -7,6 +7,8 @@ import Languages from '@shared/components/languages/Languages'
 import Theme from '@shared/components/theme/Theme'
 import ScrollUp from '@shared/components/scroll-up/ScrollUp'
 
+import { version } from '../../../package.json'
+
 const Footer = ({t}: any) => {
   const [year, setYear]: any = useState(0)
 
@@ -26,15 +28,18 @@ const Footer = ({t}: any) => {
           </div>
         </div>
         <div className={styles['bottom-wrapper']}>
-          <p className={styles.copyright}>© {year} Gabriel Lima - {t('footer.copyright')}</p>
+          <p className={styles.copyright}>
+            <span>© {year} Gabriel Lima - {t('footer.copyright')} - {t('footer.version')}</span>
+            <span> {version}</span>
+          </p>
           <p className={styles.quote}>
             <Trans i18nKey="footer.quote">
-              <span>Made with a lot of <em>anxiety</em> during </span>
+              <span>Made during the </span>
               <a
                 className={styles['quote-link']}
                 href="https://www.google.com/search?q=covid+19+march+in+2020+spain&rlz=1C1GCEA_enES841ES841&ei=7w1mYIilBsG45OUP4uWMyAk&oq=covid+19+march+in+2020+spain&gs_lcp=Cgdnd3Mtd2l6EAM6CQgAELADEAgQHjoGCAAQFhAeOggIIRAWEB0QHjoHCCEQChCgAVCYC1jbE2CrFWgCcAB4AIAB5wGIAf4LkgEFMC43LjGYAQCgAQGqAQdnd3Mtd2l6yAEIwAEB&sclient=gws-wiz&ved=0ahUKEwjI2PbQ093vAhVBHLkGHeIyA5kQ4dUDCA0&uact=5"
               >
-                a quarantine time.
+                COVID-19's quarantine.
               </a>
             </Trans>
           </p>
